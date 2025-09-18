1eba6ce4e1368a06a079e3784484f598d718b4a9
Diego M. Vadell <diego.m.vadell@gmail.com>
2025-09-18 20:16:52 +0000

    Fix tests

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -16,9 +16,10 @@ defmodule Parroquiax.DataCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
-      alias Ecto.Adapters.SQL.Sandbox
       alias Parroquiax.Repo
 
       import Ecto
